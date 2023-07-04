68f0e0c2095cdc39e4a28bf0dba9bef56c256dca
Nazarii Savorona <nazar.savorona@gmail.com>
2023-07-04 21:09:30 +0300

    Add unit test for message

---
+++ pkg/models/message_test.go
@@ -0,0 +1,38 @@
+package models_test
+
+import (
+	"github.com/GenesisEducationKyiv/main-project-nazarsavorona/pkg/models"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestNewMessage(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+		r    *models.Rate
+		want *models.Message
+	}{
+		{
+			name: "check message",
+			r: &models.Rate{
+				From: "USD",
+				To:   "UAH",
+				Rate: 27.5,
+			},
+			want: &models.Message{
+				Subject: "USD rate",
+				Body:    "1 USD = 27.50 UAH",
+			},
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+			require.Equal(t, tt.want, models.NewMessage(tt.r))
+		})
+	}
+}
