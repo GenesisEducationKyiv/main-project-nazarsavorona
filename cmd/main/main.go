3562e85490c84470c5fefbd39b735668ecfc55c7
Nazarii Savorona <nazar.savorona@gmail.com>
2023-07-06 21:34:59 +0300

    Refactor main.go

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"net/smtp"
@@ -19,28 +20,28 @@ import (
 )
 
 func main() {
-	port := os.Getenv("PORT")
-
-	smtpHost := os.Getenv("SMTP_HOST")
-	smtpPort := os.Getenv("SMTP_PORT")
-
-	senderEmail := os.Getenv("EMAIL")
-	senderPassword := os.Getenv("EMAIL_PASSWORD")
-
-	fromCurrency := os.Getenv("FROM_CURRENCY")
-	toCurrency := os.Getenv("TO_CURRENCY")
-
-	dbFileFolder := os.Getenv("DB_FILE_FOLDER")
-	dbFileName := os.Getenv("DB_FILE_NAME")
-
-	binanceURL := os.Getenv("BINANCE_API_URL")
-
-	err := os.MkdirAll(dbFileFolder, 0666)
+	envValues, err := getEnvironmentValues()
 	if err != nil {
-		log.Panicln(err.Error())
+		log.Panicf("Error fetching environment values: %v\n", err)
 	}
 
-	file, err := os.OpenFile(dbFileFolder+dbFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	port := envValues["PORT"]
+
+	smtpHost := envValues["SMTP_HOST"]
+	smtpPort := envValues["SMTP_PORT"]
+
+	senderEmail := envValues["EMAIL"]
+	senderPassword := envValues["EMAIL_PASSWORD"]
+
+	fromCurrency := envValues["FROM_CURRENCY"]
+	toCurrency := envValues["TO_CURRENCY"]
+
+	dbFileFolder := envValues["DB_FILE_FOLDER"]
+	dbFileName := envValues["DB_FILE_NAME"]
+
+	binanceURL := envValues["BINANCE_API_URL"]
+
+	file, err := prepareFile(dbFileFolder, dbFileName)
 	if err != nil {
 		log.Panicln(err.Error())
 	}
@@ -69,4 +70,44 @@ func main() {
 	if err != nil {
 		log.Panicln(err.Error())
 	}
+}
+
+func getEnvironmentValues() (map[string]string, error) {
+	requiredEnvVars := []string{
+		"PORT",
+		"SMTP_HOST",
+		"SMTP_PORT",
+		"EMAIL",
+		"EMAIL_PASSWORD",
+		"FROM_CURRENCY",
+		"TO_CURRENCY",
+		"DB_FILE_FOLDER",
+		"DB_FILE_NAME",
+		"BINANCE_API_URL",
+	}
+
+	envValues := make(map[string]string)
+
+	for _, envVar := range requiredEnvVars {
+		value := os.Getenv(envVar)
+		if value == "" {
+			return nil, fmt.Errorf("environment variable %s is not set", envVar)
+		}
+		envValues[envVar] = value
+	}
+
+	return envValues, nil
+}
+
+func prepareFile(dbFileFolder string, dbFileName string) (*os.File, error) {
+	err := os.MkdirAll(dbFileFolder, 0666)
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+
+	file, err := os.OpenFile(dbFileFolder+dbFileName, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Panicln(err.Error())
+	}
+	return file, err
 }
